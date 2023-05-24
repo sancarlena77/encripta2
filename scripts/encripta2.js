2a8086032a5c742a3bc0eb5cd922241816ee230e
Heidy <correo@privado.com>
2023-05-24 07:33:54 -0600

    Declaración inicial de variables

---
+++ scripts/encripta2.js
@@ -0,0 +1,41 @@
+//Declaración de variables//
+ 
+//Variables de interacción con el index//
+
+const textoIngresado = document.getElementById("textoIngresado"); 
+var textoProcesado = textoIngresado.value; 
+
+const encriptar = document.getElementById("encriptar");
+    encriptar.addEventListener("click", (event) => encriptarTexto(event)); 
+
+const desencriptar = document.getElementById("desencriptar");
+    desencriptar.addEventListener("click", (event) => desencriptarTexto(event)); 
+
+const copiar = document.getElementById("copiar");
+    copiar.addEventListener("click", (event) => copiarTexto()); 
+
+let mensajeVacio = document.getElementById("mensaje-vacio"); //Div que muestra personajes y mensaje de espera//
+let mensajeProcesado = document.getElementById("mensaje-procesado"); //Div que muestra elementos del resultado//
+
+var impresionParametros = document.getElementById("impresionParametros"); //Div donde se mostrará el resultado//
+var htmlParametros = ""; //Construcción de cadena para mostrar resultado//
+
+//Variables de validación//
+
+const caracteresPermitidos = /^[a-zñ\s]+$/; 
+const caracteresMinimos = /[a-zñ]/; 
+let continuarProcesamiento = true; 
+
+//Variables operativas//
+
+const llavesDeEncriptado = [ 
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"]
+];
+
+
+
+
